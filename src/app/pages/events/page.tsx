b64a36193bafb69a64e7ce2ba0f14dc12ee47009
Archie045 <mehtaarchie25@gmail.com>
2024-07-13 20:29:42 +0530

    "second commit"

--- src/app/pages/events/page.tsx
+++ src/app/pages/events/page.tsx
@@ -1,9 +1,13 @@
 
 // 
+
+import CardHoverEffectDemo from "@/components/cards";
+
 export default function Home() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white" style={{ color: 'black' }}>
-       events
+       events 
+       < CardHoverEffectDemo />
       </main>
     );
   }
